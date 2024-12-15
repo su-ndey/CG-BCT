f9914962350390652d746f72f886f3693e0c3365
Yolisa11 <yolisabhujel@gmail.com>
2024-12-15 19:16:02 +0545

    pyproject added

--- Yolisa-Bhujel/py-project/spaceshooter.py
+++ Yolisa-Bhujel/py-project/spaceshooter.py
@@ -1,27 +1,20 @@
 import math
 import random
-
 import pygame
 from pygame import mixer
-
 # Intialize the pygame
 pygame.init()
-
 # create the screen
 screen = pygame.display.set_mode((800, 600))
-
 # Background
 background = pygame.image.load('background1.png')
-
 # Sound
 mixer.music.load("background.wav")
 mixer.music.play(-1)
-
 # Caption and Icon
 pygame.display.set_caption("Space Invader")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
-
 # Player
 playerImg = pygame.image.load('player.png')
 playerX = 370
