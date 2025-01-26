cf7a2fc3ec63242c0b770e62caed5a1fe86ffd9c
Parth <sudip1718mahat@gmail.com>
2025-01-26 21:41:04 +0545

    ALl labs

---
+++ Sudip-Mahat/lab-3/lab3.py
@@ -0,0 +1,33 @@
+# Midpoint Circle Drawing Algorithm
+
+import matplotlib.pyplot as plt
+
+x =0
+r =int(input("Enter the radius of the circle: "))
+xc= int(input("Enter the x coordinate of the center: "))
+yc= int(input("Enter the y coordinate of the center: "))
+y =r
+p =1-r
+xes =[]
+yes =[]
+def plot(x,y,xc,yc):
+    xes.extend([x+ xc, -x+ xc, x+ xc, -x+ xc, y+ xc, -y+ xc, y+ xc, -y+ xc])
+    yes.extend([y+ yc, y+ yc, -y+ yc, -y+ yc, x+ yc, -x+ yc, -x+ yc, x+ yc])
+plot(x,y,xc,yc)
+while x<y:
+    x+=1
+    if p<0:
+        p =p+2*x+1
+    else:
+        y-=1
+        p = p+2*(x-y)+1
+    plot(x,y,xc,yc)
+
+plt.scatter(xes,yes)
+plt.gca().set_aspect('equal',adjustable='box')
+plt.grid(True)
+plt.savefig("output.png")
+plt.show()
+
+ 
+
